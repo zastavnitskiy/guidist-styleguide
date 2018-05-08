5aecec2561d96d350ec55ce5deef6c82b6dd2c79
zastavnitskiy <pasha.zastavnitskiy@gmail.com>
2018-05-08 20:11:40 +0200

    initial commit

---
+++ src/index.js
@@ -0,0 +1,2 @@
+import Button from './components/Button';
+console.log('Hello World!')
---
+++ webpack.config.js
@@ -0,0 +1,54 @@
+const MiniCssExtractPlugin = require("mini-css-extract-plugin");
+const path = require('path');
+
+module.exports = {
+	module: {
+		rules: [
+			{
+				test: /\.js$/,
+				exclude: /node_modules/,
+				loader: 'babel-loader',
+
+				options: {
+					presets: [['env'], ['react']]
+				}
+			},
+			{
+				test: /\.css$/,
+
+				use: [
+					{
+						loader: MiniCssExtractPlugin.loader
+					},
+					{
+						loader: 'css-loader',
+
+						options: {
+							sourceMap: true,
+							importLoaders: 1
+						}
+					},
+					{
+						loader: 'postcss-loader',
+
+						options: {
+							plugins: function() {
+								return [precss, autoprefixer];
+							}
+						}
+					}
+				]
+			}
+		]
+	},
+
+	entry: './src/index.js',
+
+	output: {
+		filename: '[name].[chunkhash].js',
+		chunkFilename: '[name].[chunkhash].js',
+		path: path.resolve(__dirname, 'dist')
+	},
+
+	mode: 'production'
+};
